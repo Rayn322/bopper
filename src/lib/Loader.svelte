52d347164d3dfdc1e9c2397508bad5892079a4cd
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2023-07-08 23:43:17 -0700

    Search bar

---
+++ src/lib/Loader.svelte
@@ -0,0 +1,15 @@
+<!-- By Sam Herbert (@sherb), for everyone. More @ http://goo.gl/7AJzbL -->
+<svg
+	class="mx-auto animate-spin stroke-gray-900"
+	width="20"
+	height="20"
+	viewBox="0 0 38 38"
+	xmlns="http://www.w3.org/2000/svg"
+>
+	<g fill="none" fill-rule="evenodd">
+		<g transform="translate(1 1)" stroke-width="2">
+			<circle stroke-opacity=".5" cx="18" cy="18" r="18" />
+			<path d="M36 18c0-9.94-8.06-18-18-18" />
+		</g>
+	</g>
+</svg>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,45 @@
-<script>
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import Loader from '$lib/Loader.svelte';
 	import '../app.css';
+
+	let songName = '';
+	let loading = false;
+
+	async function onSubmit() {
+		loading = true;
+		await goto(`/${songName}`);
+		loading = false;
+	}
 </script>
 
-<slot />
+<div class="m-4 space-y-4">
+	<div class="flex items-center justify-between">
+		<h1 class="text-2xl">
+			<a href="/">Bopper</a>
+		</h1>
+		<div>
+			<form on:submit|preventDefault={onSubmit} class="flex space-x-2">
+				<input
+					bind:value={songName}
+					disabled={loading}
+					type="text"
+					placeholder="Song name"
+					class="rounded disabled:bg-gray-200"
+				/>
+				<button
+					disabled={loading}
+					type="submit"
+					class="w-12 rounded border border-gray-500 p-2 disabled:bg-gray-200"
+				>
+					{#if loading}
+						<Loader />
+					{:else}
+						Bop
+					{/if}
+				</button>
+			</form>
+		</div>
+	</div>
+	<slot />
+</div>
